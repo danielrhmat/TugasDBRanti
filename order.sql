6383a4accea60a3c6bebb4497d1d0e7e48230b8d
encepihwan <62326462+encepihwan@users.noreply.github.com>
2020-03-19 15:36:25 +0700

    Update order.sql

--- order.sql
+++ order.sql
@@ -1,7 +1,7 @@
-/*buat tabel order
+/*buat tabel order*/
 create table order (id_order char (10), id_menu VARCHAR(20), id_pelanggan char(20), total_harga char(20), primary key(id_order));
 
-/*Input data
+/*Input data*/
 insert into order values ('O1','MM1','P2','150000');
 insert into order values ('O2','MM2','P2','150000');
 insert into order values ('O3','MM3','P2','150000');
@@ -9,8 +9,8 @@ insert into order values ('O4','MM4','P2','150000');
 insert into order values ('O5','MM5','P2','150000');
 insert into order values ('O6','MM6','P2','150000');
 
-/*Menampilkan semua data
+/*Menampilkan semua data*/
 select * from order;
 
-/*menampilkan data pada kondisi tertentu
+/*menampilkan data pada kondisi tertentu*/
 select id_order, total harga from order;
